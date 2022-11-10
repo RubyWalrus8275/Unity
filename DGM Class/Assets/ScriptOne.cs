3c787bcb8b17ec76bc760f328a02dfa2238d11a1
Taylor Brustad <10915123@uvu.edu>
2022-11-10 14:18:54 -0700

    Added script 1

--- DGM Class/Assets/ScriptOne.cs
+++ DGM Class/Assets/ScriptOne.cs
@@ -5,9 +5,20 @@ using UnityEngine;
 public class ScriptOne : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            GetComponent<Render> ().material.color = Color
+        }
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            GetComponent<Render>().material.color = Color
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            GetComponent<Render>().material.color = Color
+        }
     }
 
     // Update is called once per frame
